f9f7767ec36603d52bb959c81271e6ca2ae0abcb
alexr63 <alexr63@hotmail.com>
2023-06-20 19:42:48 +0300

    serialize and deserialize borehole

--- TestBoreholes/Program.cs
+++ TestBoreholes/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Newtonsoft.Json;
+
 var boreholes = new List<Borehole>
 {
     new Borehole(1, new Location
@@ -6,30 +8,42 @@ var boreholes = new List<Borehole>
         City = "London",
         Country = "United Kingdom"
     }, "John Doe",
-        new Pumping(150.00, 100)),
+        new Pumping(150.0, 100.00m)),
     new Borehole(2, new Location
     {
         City = "Paris",
         Country = "France"
     }, "Jane Doe",
-        new Damaged(DamageType.Major, new RequiresService(1000))),
+        new Damaged(DamageType.Major, new RequiresService(1000.00m))),
     new Borehole(3, new Location
     {
         City = "Berlin",
         Country = "Germany"
     }, "John Doe",
-               new Damaged(DamageType.Minor, new BeingRepaired(100))),
+               new Damaged(DamageType.Minor, new BeingRepaired(100.00m))),
     new Borehole(4, new Location
     {
         City = "Madrid",
         Country = "Spain"
     }, "Jane Doe",
-                      new Damaged(DamageType.Major, new BeyondRepair(2000)))
+                      new Damaged(DamageType.Major, new BeyondRepair(2000.00m)))
 };
 
 foreach (var borehole in boreholes)
 {
+    
+}
+
+foreach (var borehole in boreholes)
+{
     Console.WriteLine(borehole);
+    string json = JsonConvert.SerializeObject(borehole);
+    var settings = new JsonSerializerSettings
+    {
+        TypeNameHandling = TypeNameHandling.Auto
+    };
+    var obj = JsonConvert.DeserializeObject<Borehole>(json, settings);
+    Console.WriteLine(obj);
 }
 
 public class Location
@@ -39,14 +53,98 @@ public class Location
     // Additional properties like latitude, longitude, etc.
 }
 
-public abstract record Service();
-record RequiresService(decimal EstimatedRepairCost) : Service;
-record BeingRepaired(decimal DailyRepairCost) : Service;
-record BeyondRepair(decimal TotalRepairCost) : Service;
+public abstract class Service
+{
+    protected Service()
+    {
+    }
+}
 
-public abstract record Status;
-public record Pumping(double Volume, decimal EstimatedDailyOperationsCost) : Status;
-public record Damaged(DamageType DamageType, Service Service) : Status;
+class RequiresService : Service
+{
+    public RequiresService(decimal EstimatedRepairCost)
+    {
+        this.EstimatedRepairCost = EstimatedRepairCost;
+    }
+
+    public decimal EstimatedRepairCost { get; init; }
+
+    public void Deconstruct(out decimal EstimatedRepairCost)
+    {
+        EstimatedRepairCost = this.EstimatedRepairCost;
+    }
+}
+
+class BeingRepaired : Service
+{
+    public BeingRepaired(decimal DailyRepairCost)
+    {
+        this.DailyRepairCost = DailyRepairCost;
+    }
+
+    public decimal DailyRepairCost { get; init; }
+
+    public void Deconstruct(out decimal DailyRepairCost)
+    {
+        DailyRepairCost = this.DailyRepairCost;
+    }
+}
+
+class BeyondRepair : Service
+{
+    public BeyondRepair(decimal TotalRepairCost)
+    {
+        this.TotalRepairCost = TotalRepairCost;
+    }
+
+    public decimal TotalRepairCost { get; init; }
+
+    public void Deconstruct(out decimal TotalRepairCost)
+    {
+        TotalRepairCost = this.TotalRepairCost;
+    }
+}
+
+public abstract class Status
+{
+}
+
+public class Pumping : Status
+{
+    public Pumping(double Volume, decimal EstimatedDailyOperationsCost)
+    {
+        this.Volume = Volume;
+        this.EstimatedDailyOperationsCost = EstimatedDailyOperationsCost;
+    }
+
+    public double Volume { get; init; }
+    public decimal EstimatedDailyOperationsCost { get; init; }
+
+    public void Deconstruct(out double Volume, out decimal EstimatedDailyOperationsCost)
+    {
+        Volume = this.Volume;
+        EstimatedDailyOperationsCost = this.EstimatedDailyOperationsCost;
+    }
+}
+
+public class Damaged : Status
+{
+    public Damaged(DamageType DamageType, Service Service)
+    {
+        this.DamageType = DamageType;
+        this.Service = Service;
+    }
+
+    public DamageType DamageType { get; init; }
+    [JsonProperty(TypeNameHandling = TypeNameHandling.Objects)]
+    public Service Service { get; init; }
+
+    public void Deconstruct(out DamageType DamageType, out Service Service)
+    {
+        DamageType = this.DamageType;
+        Service = this.Service;
+    }
+}
 
 public class Borehole
 {
@@ -58,17 +156,21 @@ public class Borehole
         Status = status;
     }
 
+    [JsonProperty]
     int Id { get; init; }
+    [JsonProperty]
     Location Location { get; init; }
+    [JsonProperty]
     string Owner { get; init; }
 
+    [JsonProperty(TypeNameHandling = TypeNameHandling.Objects)]
     Status Status { get; init; }
 
     public override string ToString()
     {
         var line1 = $"Borehole {Id} is owned by {Owner} and is located in {Location.City}, {Location.Country}.";
         var line2 = Status.Format();
-        return line1 + Environment.NewLine + line2;
+        return String.Join(" ", line1, line2);
     }
 }
 
