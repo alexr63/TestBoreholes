f2ed1fba6796f74473cd779777e508dfd5e85604
alexr63 <alexr63@hotmail.com>
2023-06-21 14:42:56 +0300

    remove unused code from main program

--- TestBoreholes/Program.cs
+++ TestBoreholes/Program.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using TestBoreholes;
+﻿using TestBoreholes;
 using TestBoreholes.WaterSources;
-using TestBoreholes.WaterSources.Boreholes;
 using TestBoreholes.WaterSources.Boreholes.Statuses;
 using TestBoreholes.WaterSources.Boreholes.Statuses.Services;
 using Stream = TestBoreholes.WaterSources.Stream;
@@ -19,23 +17,6 @@ var locations = new List<Location>
     new Location("Kiev", "Ukraine", 50.4501, 30.5234, new Rain(2000)),
     new Location("Ibadan", "Nigeria", 7.3117, 3.9026, new Rain(3000)),
 };
-
-var ibadan = locations[^1];
-ibadan.WaterSource = new Borehole(6, "Ibrahim", new Pumping(300, 300));
-var ibadanBorehole = (Borehole)ibadan.WaterSource;
-TimeSpan timeZoneOffset = TimeSpan.FromHours(1);
-ibadanBorehole.AddConsumption(new Consumption(new DateTimeOffset(2021, 1, 1, 10, 30, 0, timeZoneOffset), 100));
-ibadanBorehole.AddConsumption(new Consumption(new DateTimeOffset(2021, 1, 2, 20, 30, 0, timeZoneOffset), 200));
-Console.WriteLine(JsonConvert.SerializeObject(ibadan));
-
-const string targetTimeZoneId = "FLE Standard Time";
-foreach (var consumption in ibadanBorehole.Consumptions)
-{
-    var dateTimeOffset = consumption.DateTimeOffset;
-    var dateTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTimeOffset, targetTimeZoneId);
-    Console.WriteLine($"{dateTimeOffset} UTC is {dateTime} in {targetTimeZoneId}");
-    Console.WriteLine($"Consumption at {dateTime} is {ibadanBorehole.GetConsumption(dateTime)}");
-}
 
 foreach (var location in locations)
 {
