53d449ade6c34cba73ba13d2c14f2bc69f7e98ce
alexr63 <alexr63@hotmail.com>
2023-06-22 15:54:09 +0300

    improve SerializationTest

--- TestBoreholes.Tests/SerializationTest.cs
+++ TestBoreholes.Tests/SerializationTest.cs
@@ -7,26 +7,21 @@ namespace TestBoreholes.Tests
 {
     public class SerializationTest
     {
-        private readonly Location _ibadan;
-
-        public SerializationTest()
-        {
-            _ibadan = new Location("Ibadan", "Nigeria", 7.3117, 3.9026, new Borehole("NG-OY-1353", "FairAction Nigeria",
-                new Pumping(300, 400)));
-        }
+        private readonly Location _ibadan = new Location("Ibadan", "Nigeria", 7.3117, 3.9026, new Borehole("NG-OY-1353", "FairAction Nigeria",
+            new Pumping(300, 400)));
+        private const string _ibadanJson = "{\"City\":\"Ibadan\",\"Country\":\"Nigeria\",\"Latitude\":7.3117,\"Longitude\":3.9026,\"WaterSource\":{\"$type\":\"TestBoreholes.WaterSources.Borehole, TestBoreholes\",\"Id\":\"NG-OY-1353\",\"Owner\":\"FairAction Nigeria\",\"Status\":{\"$type\":\"TestBoreholes.WaterSources.Boreholes.Statuses.Pumping, TestBoreholes\",\"FlowRate\":300.0,\"EstimatedDailyOperationsCost\":{\"amount\":\"400\",\"currency\":\"USD\"}},\"Consumptions\":{},\"Services\":[]}}";
 
         [Fact]
         public void SerializeLocation()
         {
             var json = JsonConvert.SerializeObject(_ibadan);
-            json.Should().Be("{\"City\":\"Ibadan\",\"Country\":\"Nigeria\",\"Latitude\":7.3117,\"Longitude\":3.9026,\"WaterSource\":{\"$type\":\"TestBoreholes.WaterSources.Borehole, TestBoreholes\",\"Id\":\"NG-OY-1353\",\"Owner\":\"FairAction Nigeria\",\"Status\":{\"$type\":\"TestBoreholes.WaterSources.Boreholes.Statuses.Pumping, TestBoreholes\",\"FlowRate\":300.0,\"EstimatedDailyOperationsCost\":{\"amount\":\"400\",\"currency\":\"USD\"}},\"Consumptions\":{},\"Services\":[]}}");
+            json.Should().Be(_ibadanJson);
         }
 
         [Fact]
         public void DeserializeLocation()
         {
-            var json = "{\"City\":\"Ibadan\",\"Country\":\"Nigeria\",\"Latitude\":7.3117,\"Longitude\":3.9026,\"WaterSource\":{\"$type\":\"TestBoreholes.WaterSources.Borehole, TestBoreholes\",\"Id\":\"NG-OY-1353\",\"Owner\":\"FairAction Nigeria\",\"Status\":{\"$type\":\"TestBoreholes.WaterSources.Boreholes.Statuses.Pumping, TestBoreholes\",\"FlowRate\":300.0,\"EstimatedDailyOperationsCost\":{\"amount\":\"400\",\"currency\":\"USD\"}},\"Consumptions\":{},\"Services\":[]}}";
-            var location = JsonConvert.DeserializeObject<Location>(json);
+            var location = JsonConvert.DeserializeObject<Location>(_ibadanJson);
             location.Should().BeEquivalentTo(_ibadan);
         }
     }
