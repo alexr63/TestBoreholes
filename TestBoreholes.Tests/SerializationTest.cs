121b754b436af712f410dea94f71c89db99abcfe
alexr63 <alexr63@hotmail.com>
2023-06-22 16:16:17 +0300

    use Money, display currency symbols correctly

--- TestBoreholes.Tests/SerializationTest.cs
+++ TestBoreholes.Tests/SerializationTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Newtonsoft.Json;
+using NodaMoney;
 using TestBoreholes.WaterSources;
 using TestBoreholes.WaterSources.Boreholes.Statuses;
 
@@ -8,8 +9,8 @@ namespace TestBoreholes.Tests
     public class SerializationTest
     {
         private readonly Location _ibadan = new Location("Ibadan", "Nigeria", 7.3117, 3.9026, new Borehole("NG-OY-1353", "FairAction Nigeria",
-            new Pumping(300, 400)));
-        private const string _ibadanJson = "{\"City\":\"Ibadan\",\"Country\":\"Nigeria\",\"Latitude\":7.3117,\"Longitude\":3.9026,\"WaterSource\":{\"$type\":\"TestBoreholes.WaterSources.Borehole, TestBoreholes\",\"Id\":\"NG-OY-1353\",\"Owner\":\"FairAction Nigeria\",\"Status\":{\"$type\":\"TestBoreholes.WaterSources.Boreholes.Statuses.Pumping, TestBoreholes\",\"FlowRate\":300.0,\"EstimatedDailyOperationsCost\":{\"amount\":\"400\",\"currency\":\"USD\"}},\"Consumptions\":{},\"Services\":[]}}";
+            new Pumping(300, new Money(76.54m, "NGN"))));
+        private const string _ibadanJson = "{\"City\":\"Ibadan\",\"Country\":\"Nigeria\",\"Latitude\":7.3117,\"Longitude\":3.9026,\"WaterSource\":{\"$type\":\"TestBoreholes.WaterSources.Borehole, TestBoreholes\",\"Id\":\"NG-OY-1353\",\"Owner\":\"FairAction Nigeria\",\"Status\":{\"$type\":\"TestBoreholes.WaterSources.Boreholes.Statuses.Pumping, TestBoreholes\",\"FlowRate\":300.0,\"EstimatedDailyOperationsCost\":{\"amount\":\"76.54\",\"currency\":\"NGN\"}},\"Consumptions\":{},\"Services\":[]}}";
 
         [Fact]
         public void SerializeLocation()
